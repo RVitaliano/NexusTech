0b7937126f9e5c51dec6236c81f831955efbd2b3
RVitaliano <bebetto1610@gmail.com>
2026-03-26 11:47:37 -0300

    update backend

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,2 +1,63 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+from app.database import get_db
+from app.models import Usuario
+from app.schemas import UsuarioCreate, UsuarioResponse, Token
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 router = APIRouter()
+
+def hash_senha(senha: str) -> str:
+    return pwd_context.hash(senha)
+
+def verificar_senha(senha: str, hash: str) -> bool:
+    return pwd_context.verify(senha, hash)
+
+def criar_token(dados: dict) -> str:
+    dados_copy = dados.copy()
+    expira = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    dados_copy.update({"exp": expira})
+    return jwt.encode(dados, SECRET_KEY, algorithm=ALGORITHM)
+
+@router.post("/registro", response_model=UsuarioResponse, status_code=201)
+def registro(usuario: UsuarioCreate, db: Session = Depends(get_db)):
+
+    existe = db.query(Usuario).filter(Usuario.email == usuario.email).first()
+    if existe:
+        raise HTTPException(status_code=400, detail="Email já Cadastrado")
+
+    novo_usuario = Usuario(
+        nome=usuario.nome,
+        email=usuario.email,
+        senha_hash=hash_senha(usuario.senha)
+    )
+    db.add(novo_usuario)
+    db.commit()
+    db.refresh(novo_usuario)
+    return novo_usuario
+
+@router.post("/login", response_model=Token)
+def login(dados: UsuarioCreate, db: Session = Depends(get_db)):
+    usuario + db.query(Usuario).filter(Usuario.email == dados.email).first()
+
+    if not usuario or not verificar_senha(dados.senha, usuario.senha_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Email ou Senha incorreta"
+        )
+
+    token = criar_token({"sub": usuario.email})
+    return {"acess_token": token, "token_type": "bearer"}
