82d561b3cabd3593be9ae7bb273606c24154b0b2
RVitaliano <bebetto1610@gmail.com>
2026-03-27 02:32:03 -0300

    frontend conectado - login com API real

--- frontend/src/app/context/AuthContext.tsx
+++ frontend/src/app/context/AuthContext.tsx
@@ -13,3 +13,54 @@ interface AuthContextType {
   logout: () => void;
 }
 
+const AuthContext = createContext<AuthContextType | undefined >(undefined);
+
+export function AuthProvider({ children }: { children:ReactNode }){
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem('token')
+  );
+
+  const [user, setUser] = useState<User | null>(() => {
+    const saved = localStorage.getItem('user');
+    return saved ? JSON.parse(saved) : null;
+  });
+  
+  const login = async (email: string, password: string) => {
+    const response = await api.post('/auth/login', {
+      nome: '',
+      email,
+      senha: password,
+    });
+    
+    const { access_token } = response.data;
+    
+    localStorage.setItem('token', access_token);
+
+    const userData = {name: 'Admin NexusTech', email};
+    localStorage.setItem('user', JSON.stringify(userData));
+
+    setUser(userData);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setIsAuthenticated(false);
+    setUser(null);
+  }
+
+  return(
+    <AuthContext.Provider value={{isAuthenticated, user, login, logout}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth(){
+  const context = useContext(AuthContext);
+  if (!context){
+    throw new Error('useAuth must be used within AuthProvider')
+  }
+  return context;
+}
